55532ed07c51615e53e90cb1c7488679b56e2d30
mdgordo <mdgordo@gmail.com>
2017-12-16 21:26:18 -0500

    Update and rename script3.m to SVM_examples.m

--- SVM_examples.m
+++ SVM_examples.m
@@ -1,13 +1,15 @@
 %%  
-% Linear SVM testing dat 
+% Linear SVM testing data 
 data = load('simple_iris.mat');
 x = data.x;
 c = data.c;
 kh = @(x,y) x*y';
-%%
+
+%% Train linear SVM classifier on IRIS data
 Md = svm_train(c,x,kh);
 [chat, d]=svm_classify(Md,x)
-%% Plot details
+
+%% Plot data colored by class
 red=chat==-1
 blue=chat==1
 [mn, in]=max(d(red))
@@ -18,6 +20,7 @@ colors(blue,:)=repmat([0 0 1],sum(blue),1)
 colors(in,:)=[0 0 0]
 colors(ix,:)=[0 0 0]
 scatter(x(:,1),x(:,2),36,colors)
+
 %% plot support vectors
 j=1
     for i=1:size(x,1)
@@ -38,17 +41,16 @@ fimplicit(@(x,y) w(1)*x+w(2)*y-Md.beta+1)
 xlim([4 7.5])
 ylim([1.5 5])
 print('svmscatter1.png', '-dpng')
- %%
-% Quadratic kernel SVM testing data
-clear all
 
+%% Quadratic kernel SVM testing data
+clear all
 data = load('simple_nonlinear.mat');
 x = data.x;
 c = data.c;
 kh = @(x,y) (x*y'+1)^2
 Md = svm_train(c,x,kh);
 
-%% create points
+%% Visualize decision areas
 BB=[max(x(:,1)) max(x(:,2)); min(x(:,1)) min(x(:,2))];
 ranges=[BB(1,1)-BB(2,1) BB(1,2)-BB(2,2)]
 for i=1:20
@@ -63,10 +65,11 @@ for i=1:20
         y(a,:)=[X(1,i) Y(j,1)]
     end
 end
-%%
+%% classify meshgrid points
 [chat, d]=svm_classify(Md,y)
 scatter(y(:,1),y(:,2),36,chat) 
-%% plot details
+
+%% create plot
 red=chat==-1
 blue=chat==1
 [mn, in]=max(d(red))
@@ -78,7 +81,8 @@ colors(in,:)=[0 0 0]
 colors(ix,:)=[0 0 0]
 scatter(y(:,1),y(:,2),36,colors)
 print('svmscatter2.png', '-dpng')
-%%
+
+%% Show decision areas by confidence
 scatter(y(:,1),y(:,2),36,d) 
 print('svmscatter3.png', '-dpng')
 
